3aeca825de2ba4dbba8ddde90f7f2f413ea470ec
Ali <alibek24galaxy@gmail.com>
2022-01-28 15:47:17 +0600

    ObjectDetailMixin.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse
+from django.views.generic import View
+from django.shortcuts import (render, get_object_or_404, redirect)
 from django.shortcuts import render
 
 
 # Create your views here.
 from .models import *
+from .utils import ObjectDetailMixin
 
 
 def posts_list(request):
@@ -11,19 +13,19 @@ def posts_list(request):
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
 
 
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
-
-
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
 
 
 
