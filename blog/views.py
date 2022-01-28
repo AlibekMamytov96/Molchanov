65015fd55f070299391979d265a346a0044c08e1
Ali <alibek24galaxy@gmail.com>
2022-01-28 14:58:55 +0600

    create models, post_detail template.

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from .models import Post
+
+
 def posts_list(request):
-    n = ['Makers', 'Bootcamp', 'python']
-    return render(request, 'blog/index.html', context={'names':n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
